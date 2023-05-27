c10d4f4d99f3664379a16b1a9bd4d67bc18c7a5d
SeifYehia <SeifYehiaDev@outlook.com>
2023-05-27 21:29:25 +0300

    Swap two numbers way three

---
+++ Sessions/Task_003/Swap_V3.c
@@ -0,0 +1,47 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/05/2023
+ * */
+
+#include <stdio.h>
+
+//                                                 _Question_
+                                        /*Swap two numbers way Three*/
+//                                                 _Solution_
+void main() {
+    int firstNumber = 5, secondNumber = 4;
+    printf("Before Swap:\n");
+    printf("First number is %d and second number is %d\n", firstNumber, secondNumber);
+    firstNumber = firstNumber ^ secondNumber;
+    /*
+     * 5     = 0 1 0 1
+     * 4     = 0 1 0 0
+     * 5 ^ 4 = (0 0 0 1) ------> 1
+     * firstNumber = 1
+     * */
+    secondNumber = firstNumber ^ secondNumber;
+    /*
+     *  1     = 0 0 0 1
+     *  4     = 0 1 0 0
+     *  1 ^ 4 = 0 1 0 1 ------> 5
+     *  secondNumber = 5
+     * */
+    firstNumber = firstNumber ^ secondNumber;
+    /*
+     *  1     = 0 0 0 1
+     *  5     = 0 1 0 1
+     *  1 ^ 5 = 0 1 0 0 ------> 4
+     *  firstNumber = 4
+     * */
+    printf("After Swap:\n");
+    printf("First number is %d and second number is %d\n", firstNumber, secondNumber);
+}
+
+//                                                 _Output_
+/*
+Before Swap:
+First number is 5 and second number is 4
+After Swap:
+First number is 4 and second number is 5
+ */
