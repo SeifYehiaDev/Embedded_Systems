b9050f247863c3c03693c1a3c570f551e5b1f671
SeifYehia <SeifYehiaDev@outlook.com>
2023-05-27 23:43:37 +0300

    Reminder without using operation

---
+++ Sheets/Sheet_01/Question_05.c
@@ -0,0 +1,30 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/05/2023
+ * */
+
+#include <stdio.h>
+
+//                                                 _Question_
+        /*Write a program that take two numbers and calculate the reminder without using % operation.*/
+//                                                 _Solution_
+void main() {
+    int firstNumber, secondNumber;
+    printf("Enter two integers to calculate reminder:  ");
+    scanf("%d %d", &firstNumber, &secondNumber);
+    (secondNumber == 0) ? printf("Error: You can't calculate the reminder by zero") :
+    printf("%d %% %d = %d\n",firstNumber, secondNumber,firstNumber -(firstNumber /secondNumber *secondNumber));
+}
+
+//                                                 _Output One_
+/*
+Enter two integers to calculate reminder:  5 0
+Error: You can't calculate the reminder by zero
+ */
+
+//                                                 _Output Two_
+/*
+Enter two integers to calculate reminder:  10 8
+10 % 8 = 2
+ */
