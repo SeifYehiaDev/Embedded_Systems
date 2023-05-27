1404657ba01e35b5ae7bbefd9da92517e6ac0585
SeifYehia <SeifYehiaDev@outlook.com>
2023-05-28 02:33:52 +0300

    Calculate sum numbers

--- Sheets/Sheet_01/Question_03.c
+++ Sheets/Sheet_01/Question_03.c
@@ -10,17 +10,22 @@
 /*Write a program to take numbers from user and calculate the sum of them (let users choose the No of numbers).*/
 //                                                 _Solution_
 void main() {
-    int number, totalNumber, index, sum = 0;
+    int number, totalNumber, sum = 0;
+    unsigned index;
     printf("Enter the total numbers: ");
     scanf("%d", &totalNumber);
+    if (totalNumber < 0) {
+        printf("Error: the total number should be positive number.");
+        return;
+    }
     for (index = 1; index <= totalNumber; sum += number, index++) {
-        printf("Enter number %d: ", index);
+        printf("Enter number %u: ", index);
         scanf("%d", &number);
     }
-    printf("The sum of numbers is %d", sum);
+    printf("The sum of numbers is %d.", sum);
 }
 
-//                                                 _Output_
+//                                                 _Output One_
 /*
 Enter the total numbers: 5
 Enter number 1: 2
@@ -28,5 +33,11 @@ Enter number 2: 3
 Enter number 3: 4
 Enter number 4: 5
 Enter number 5: 6
-The sum of numbers is 20
+The sum of numbers is 20.
+ */
+
+//                                                 _Output Two_
+/*
+Enter the total numbers: -5
+Error: the total number should be positive number.
  */
