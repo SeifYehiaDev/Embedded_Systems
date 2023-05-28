9eecbd45edec7124cfeaa17266a47c2c243c54ce
SeifYehia <SeifYehiaDev@outlook.com>
2023-05-28 06:55:14 +0300

    Sum of numbers

---
+++ Sheets/Sheet_01/Question_01.c
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/05/2023
+ * */
+
+#include <stdio.h>
+
+//                                                 _Question_
+        /*Write a program to take 2 numbers from user and calculate sum of all numbers between them.*/
+//                                                 _Solution_
+void main() {
+    int firstNumber, secondNumber, sum = 0, index;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &firstNumber, &secondNumber);
+    if (firstNumber > secondNumber) firstNumber = (firstNumber + secondNumber) - (secondNumber = firstNumber);
+    for (index = firstNumber + 1; index < secondNumber; sum += index, index++);
+    printf("The sum of numbers between %d and %d is %d.", firstNumber, secondNumber, sum);
+}
+
+//                                                 _Output_
+/*
+Enter two numbers: 6 -5
+The sum of numbers between -5 and 6 is 5.
+ */
