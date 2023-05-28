26b0e4bb253a8f64ede62cf12e8a0e4cccd2a13f
SeifYehia <SeifYehiaDev@outlook.com>
2023-05-28 05:22:42 +0300

    Number base of two

---
+++ Sheets/Sheet_01/Question_10.c
@@ -0,0 +1,43 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 28/05/2023
+ * */
+
+#include <stdio.h>
+#include <math.h>
+
+//                                                 _Question_
+/*Write a program that reads a positive integer and check if this number is a base of 2 like 1,2,4,8,16,32, 64....*/
+//                                                 _Solution_
+void main() {
+    signed number;
+    unsigned char power, isBaseOfTwo = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+    if (number < 0) {
+        printf("Error: the number should be a positive number.");
+        return;
+    }
+    for (power = 0; pow(2, power) <= number; power++)
+        if (pow(2, power) == number) isBaseOfTwo = 1;
+    printf("%d %s a base of 2", number, (isBaseOfTwo) ? "is" : "is not");
+}
+
+//                                                 _Output One_
+/*
+Enter a positive integer: 128
+128 is a base of 2
+ */
+
+//                                                 _Output Two_
+/*
+Enter a positive integer: 129
+129 is not a base of 2
+ */
+
+//                                                 _Output Three_
+/*
+Enter a positive integer: -128
+Error: the number should be a positive number.
+ */
