6206793a28710365aad6c8252107e85e7fbba425
SeifYehia <SeifYehiaDev@outlook.com>
2023-06-10 02:17:47 +0300

    Multiply without using operation

--- Sheets/Sheet_01/Question_04.c
+++ Sheets/Sheet_01/Question_04.c
@@ -5,25 +5,29 @@
  * */
 
 #include <stdio.h>
-#include <math.h>
+int abs(int number);
 
 //                                                 _Question_
             /*Write a program that take two numbers and multiply them without using * operation.*/
 //                                                 _Solution_
 void main() {
-    int firstNumber, secondNumber;
-    long long product = 0L;
-    unsigned index;
+    signed int firstNumber, secondNumber;
+    signed long long product = 0L;
+    unsigned int index;
     unsigned char isNegative;
     printf("Enter two integers to multiply: ");
     scanf("%d %d", &firstNumber, &secondNumber);
     printf("%d * %d = ", firstNumber, secondNumber);
-    isNegative = (firstNumber < 0 && secondNumber > 0 || firstNumber > 0 && secondNumber < 0) ? 1 : 0;
+    isNegative = firstNumber < 0 && secondNumber > 0 || firstNumber > 0 && secondNumber < 0;
     firstNumber = abs(firstNumber);
     secondNumber = abs(secondNumber);
     for (index = 1; index <= ((firstNumber < secondNumber) ? firstNumber : secondNumber); index++)
         product += (firstNumber > secondNumber) ? firstNumber : secondNumber;
     printf("%lld\n", (isNegative) ? -product : product);
+}
+
+int abs(int number) {
+    return (number < 0) ? number * -1 : number;
 }
 
 //                                                 _Output One_
