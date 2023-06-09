20db777bd5010a962a56804587d0ea0885cc2d23
SeifYehia <SeifYehiaDev@outlook.com>
2023-06-10 02:50:46 +0300

    Reminder without using operation

--- Sheets/Sheet_01/Question_05.c
+++ Sheets/Sheet_01/Question_05.c
@@ -5,50 +5,59 @@
  * */
 
 #include <stdio.h>
+int abs(int number);
 
 //                                                 _Question_
         /*Write a program that take two numbers and calculate the reminder without using % operation.*/
 //                                                 _Solution_
 void main() {
-    int firstNumber, secondNumber;
-    printf("Enter two integers to calculate reminder: ");
+    signed int firstNumber, secondNumber, remainder;
+    unsigned char isNegative;
+    printf("Enter two integers: ");
     scanf("%d %d", &firstNumber, &secondNumber);
-    (secondNumber == 0) ? printf("Error: You can't calculate the reminder by zero") :
-    printf("%d %% %d = %d\n", firstNumber, secondNumber, firstNumber - (firstNumber / secondNumber * secondNumber));
+    if (!secondNumber) {
+        printf("Error: you cannot divide by zero.");
+        return;
+    }
+    printf("%d %% %d = ", firstNumber, secondNumber);
+    isNegative = firstNumber < 0;
+    firstNumber = abs(firstNumber);
+    secondNumber = abs(secondNumber);
+    remainder = firstNumber;
+    while (remainder >= secondNumber) remainder -= secondNumber;
+    printf("%d", (isNegative) ? -remainder : remainder);
+}
+
+int abs(int number) {
+    return (number < 0) ? number * -1 : number;
 }
 
 //                                                 _Output One_
 /*
-Enter two integers to calculate reminder: 10 8
+Enter two integers: 10 8
 10 % 8 = 2
  */
 
 //                                                 _Output Two_
 /*
-Enter two integers to calculate reminder: -10 8
--10 % 8 = -2
+Enter two integers: -10 -8
+-10 % -8 = -2
  */
 
 //                                                 _Output Three_
 /*
-Enter two integers to calculate reminder: 10 -8
-10 % -8 = 2
+Enter two integers: -10 8
+-10 % 8 = -2
  */
 
 //                                                 _Output Four_
 /*
-Enter two integers to calculate reminder: -10 -8
--10 % -8 = -2
+Enter two integers: 10 -8
+10 % -8 = 2
  */
 
 //                                                 _Output Five_
 /*
-Enter two integers to calculate reminder: -10 -2
--10 % -2 = 0
- */
-
-//                                                 _Output Six_
-/*
-Enter two integers to calculate reminder: 10 0
-Error: You can't calculate the reminder by zero
+Enter two integers: 10 0
+Error: you cannot divide by zero.
  */
