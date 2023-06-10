b5c7bfba51e342e0b23a85e544e992e5b08b1a5f
SeifYehia <SeifYehiaDev@outlook.com>
2023-06-10 03:42:42 +0300

    Perfect square

--- Sheets/Sheet_01/Question_09.c
+++ Sheets/Sheet_01/Question_09.c
@@ -5,26 +5,29 @@
  * */
 
 #include <stdio.h>
-#include <math.h>
 
 //                                                 _Question_
             /*Write a program that reads a positive integer and checks if it is a perfect square.*/
 //                                                 _Solution_
 void main() {
-    signed number;
+    signed int number;
+    unsigned int index;
+    unsigned char isPerfectSquare = 0;
     printf("Enter a positive integer: ");
     scanf("%d", &number);
     if (number < 0) {
         printf("Error: the number should be a positive number.");
         return;
     }
-    printf("%d %s a perfect square.", number, (pow(sqrt(number), 2) == number) ? "is" : "is not");
+    for (index = 1; index * index <= number && !isPerfectSquare; index++)
+        if (index * index == number) isPerfectSquare = 1;
+    printf("%02d %s a perfect square.", number, (isPerfectSquare) ? "is" : "is not");
 }
 
 //                                                 _Output One_
 /*
 Enter a positive integer: 9
-9 is a perfect square.
+09 is a perfect square.
  */
 
 //                                                 _Output Two_
