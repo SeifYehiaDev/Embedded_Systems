d8d93e382de1d1070e6e23fc52c1898ea220ed01
SeifYehia <SeifYehiaDev@outlook.com>
2023-06-10 03:40:10 +0300

    Perfect square

--- Sheets/Sheet_01/Question_08.c
+++ Sheets/Sheet_01/Question_08.c
@@ -7,37 +7,37 @@
 #include <stdio.h>
 
 //                                                 _Question_
-                    /*Write a program that reads a positive integer and checks if it is a prime.*/
+            /*Write a program that reads a positive integer and checks if it is a perfect square.*/
 //                                                 _Solution_
 void main() {
     signed int number;
-    unsigned int divisor;
-    unsigned char isPrime = 1;
+    unsigned int index;
+    unsigned char isPerfectSquare = 0;
     printf("Enter a positive integer: ");
     scanf("%d", &number);
     if (number < 0) {
         printf("Error: the number should be a positive number.");
         return;
     }
-    for (divisor = 2; divisor <= number / 2 && isPrime; divisor++)
-        if (number % divisor == 0) isPrime = 0;
-    printf("%d %s a prime number.", number, (isPrime) ? "is" : "isn't");
+    for (index = 1; index * index <= number && !isPerfectSquare; index++)
+        if (index * index == number) isPerfectSquare = 1;
+    printf("%02d %s a perfect square.", number, (isPerfectSquare) ? "is" : "is not");
 }
 
 //                                                 _Output One_
 /*
-Enter a positive integer: 227
-227 is a prime number.
+Enter a positive integer: 9
+09 is a perfect square.
  */
 
 //                                                 _Output Two_
 /*
-Enter a positive integer: 228
-228 isn't a prime number.
+Enter a positive integer: 10
+10 is not a perfect square.
  */
 
 //                                                 _Output Three_
 /*
-Enter a positive integer: -228
+Enter a positive integer: -10
 Error: the number should be a positive number.
  */
