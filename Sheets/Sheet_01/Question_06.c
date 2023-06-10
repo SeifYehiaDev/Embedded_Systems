ad7320d7a0fd2668e743d55f6c3917f3ed70b382
SeifYehia <SeifYehiaDev@outlook.com>
2023-06-10 03:08:13 +0300

    Calculate the power of a number

--- Sheets/Sheet_01/Question_06.c
+++ Sheets/Sheet_01/Question_06.c
@@ -5,26 +5,24 @@
  * */
 
 #include <stdio.h>
-#include <math.h>
 
 //                                                 _Question_
-/*Write a program to calculate the power of a number, The number and its power are input from user.*/
+        /*Write a program to calculate the power of a number, The number and its power are input from user.*/
 //                                                 _Solution_
 void main() {
     float number, power;
-    double result = 1.0;
-    unsigned index;
+    double result = 1.0, index;
     printf("Enter the base number: ");
     scanf("%f", &number);
     printf("Enter the power: ");
     scanf(" %f", &power);
-    for (index = 1; index <= abs(power); result *= number, index++);
-    printf("%.2f power %.2f = %.6lf", number, power, (power > 0) ? result : 1.0 / result);
+    for (index = 1.0; index <= ((power < 0) ? power * -1 : power); result *= number, index++);
+    printf("%.1f power %.1f = %.2f", number, power, (power > 0) ? result : 1.0 / result);
 }
 
 //                                                 _Output_
 /*
-Enter the base number: -2
+Enter the base number: -2 
 Enter the power: -5
--2.00 power -5.00 = -0.031250
+-2.0 power -5.0 = -0.03
  */
