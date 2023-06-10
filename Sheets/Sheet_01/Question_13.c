80ebfbcd616f12d15d6fe369ecd2b1fbc30885ae
SeifYehia <SeifYehiaDev@outlook.com>
2023-06-10 04:36:24 +0300

    Sum of holes

--- Sheets/Sheet_01/Question_13.c
+++ Sheets/Sheet_01/Question_13.c
@@ -10,25 +10,26 @@
         /*You are designing a poster which prints out numbers with a unique style applied to each of them.*/
 //                                                 _Solution_
 void main() {
-    signed number, sumOfHoles = 0, digit;
+    signed int number, sumOfHoles = 0;
+    unsigned char digit;
     printf("Enter a positive integer: ");
     scanf("%d", &number);
     if (number < 0) {
         printf("Error: the number should be a positive number.");
         return;
     }
-    while (number > 0) {
+    while (number) {
         digit = number % 10;
         sumOfHoles += (digit == 8) ? 2 : (digit == 0 || digit == 4 || digit == 6 || digit == 9) ? 1 : 0;
         number /= 10;
     }
-    printf("Number of holes are %d", sumOfHoles);
+    printf("Number of holes are %d.", sumOfHoles);
 }
 
 //                                                 _Output One_
 /*
 Enter a positive integer: 3824
-Number of holes are 3
+Number of holes are 3.
  */
 
 //                                                 _Output Two_
