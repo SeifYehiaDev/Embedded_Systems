369b6c4fc32d426d0ec7bcf11d600437b3c80737
SeifYehia <SeifYehiaDev@outlook.com>
2023-06-10 04:32:53 +0300

    Sum of even

--- Sheets/Sheet_01/Question_12.c
+++ Sheets/Sheet_01/Question_12.c
@@ -10,13 +10,14 @@
 /*write a program to take even numbers from user and print the sum of them after each entry if the user enters 2 odd number the program print "bye" and stopped.*/
 //                                                 _Solution_
 void main() {
-    signed number, sumOfEven = 0;
+    signed int number;
+    unsigned int sumOfEven = 0;
     unsigned char numberOFOdd = 0;
     do {
         printf("Enter an even number: ");
         scanf("%d", &number);
         if (number % 2 == 0)
-            printf("The sum is %d\n", sumOfEven += number);
+            printf("The sum is %u\n", sumOfEven += number);
         else
             numberOFOdd++;
     } while (numberOFOdd < 2);
@@ -25,15 +26,17 @@ void main() {
 
 //                                                 _Output_
 /*
-Enter an even number: 2
-The sum is 2
+Enter an even number: 2147483646
+The sum is 2147483646
 Enter an even number: 4
-The sum is 6
-Enter an even number: 5
+The sum is 2147483650
+Enter an even number: 1
+Enter an even number: 2
+The sum is 2147483652
 Enter an even number: 8
-The sum is 14
-Enter an even number: 10
-The sum is 24
-Enter an even number: 3
+The sum is 2147483660
+Enter an even number: 10000000
+The sum is 2157483660
+Enter an even number: 9
 bye
  */
