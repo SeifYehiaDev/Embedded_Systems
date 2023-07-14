39e9801497bc23ff9ddddb1bf9860cb066876b17
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2023-07-14 05:25:13 +0300

    Count the number of 1’s in an unsigned 32-bit integer.

---
+++ Sheets/Sheet_02/Question_04.c
@@ -0,0 +1,48 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 14/07/2023
+ * */
+
+#include <stdio.h>
+
+//                                                 _Question_
+                /*Write a program to count the number of 1’s in an unsigned 32-bit integer.*/
+//                                                 _Solution_
+// This function work with positive number only
+int countOnesInBinary_V1(int number) {
+    unsigned char count = 0;
+    while (number) {
+        if (number & 1) count++;
+        number >>= 1;
+    }
+    return count;
+}
+// This function work with positive number and negative number.
+int countOnesInBinary_V2(int number) {
+    unsigned char count = 0, bit;
+    for (bit = 0; bit < sizeof(number) * 8 - 1; bit++)
+        if ((number >> bit) & 1) count++;
+    return count;
+}
+
+void main() {
+    signed int number;
+    printf("Enter an integer number: ");
+    scanf("%d", &number);
+    printf("The number of ones in %d is %d.", number,
+           (number > 0) ? countOnesInBinary_V1(number) : countOnesInBinary_V2(number));
+}
+
+
+//                                                 _Output One_
+/*
+Enter a positive number: 255
+The number of ones in 255 is 8.
+ */
+
+//                                                 _Output Two_
+/*
+Enter an integer number: -1
+The number of ones in -1 is 31.
+ */
