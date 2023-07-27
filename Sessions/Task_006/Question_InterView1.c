e2a111d89b81b6acd33c71ace49b3c4016094215
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2023-07-27 17:22:58 +0300

    Question_01 interview

---
+++ Sessions/Task_006/Question_InterView1.c
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/07/2023
+ * */
+
+#include <stdio.h>
+
+//                                                 _Question_
+                                            /*Question interview.*/
+//                                                 _Solution_
+
+int main() {
+    // What is condition put in if condition to print Hello World!?
+    if (!printf("Hello "))
+        printf("Hello");
+    else
+        printf("World!");
+    return 0;
+}
+
+//                                                 _Output_
+/*
+Hello World!
+ */
