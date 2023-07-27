9d87364fcfeca18bcb392fcd388dc4979bc37915
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2023-07-27 17:23:09 +0300

    Question_02 interview

---
+++ Sessions/Task_006/Question_InterView2.c
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/07/2023
+ * */
+
+#include <stdio.h>
+
+//                                                 _Question_
+                                            /*Question interview.*/
+//                                                 _Solution_
+
+int main() {
+    printf("%d",printf("%d", printf("%d", printf("Hello World")))); //Hello World1121
+    return 0;
+}
+
+//                                                 _Output_
+/*
+Hello World1121
+ */
