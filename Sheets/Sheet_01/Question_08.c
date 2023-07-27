9f091558f80626908e1e2e83ea9e2360f2064fef
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2023-07-27 11:27:52 +0300

    Prime number

--- Sheets/Sheet_01/Question_08.c
+++ Sheets/Sheet_01/Question_08.c
@@ -1,43 +1,43 @@
 /*
  * Author : Seif Yehia Esmail AbdelAal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 28/05/2023
+ * Created on : 27/07/2023
  * */
 
 #include <stdio.h>
 
 //                                                 _Question_
-                    /*Write a program that reads a positive integer and checks if it is a prime.*/
+                /*Write a program that reads a positive integer and checks if it is a prime.*/
 //                                                 _Solution_
-void main() {
+int isPrimeNumber(int number) {
+    int isPrime = number > 2;
+    for (int divisor = 2; divisor <= number / 2 && isPrime; divisor++)
+        if (number % divisor == 0)isPrime = 0;
+    return isPrime;
+}
+
+int main() {
     signed int number;
-    unsigned int divisor;
-    unsigned char isPrime = 1;
-    printf("Enter a positive integer: ");
+    printf("Enter a number: ");
     scanf("%d", &number);
-    if (number < 0) {
-        printf("Error: the number should be a positive number.");
-        return;
-    }
-    for (divisor = 2; divisor <= number / 2 && isPrime; divisor++)
-        if (number % divisor == 0) isPrime = 0;
-    printf("%d %s a prime number.", number, (isPrime) ? "is" : "isn't");
+    printf("%d %s a prime number.", number, (isPrimeNumber(number)) ? "is" : "isn't");
+    return 0;
 }
 
 //                                                 _Output One_
 /*
-Enter a positive integer: 227
+Enter a number: 227
 227 is a prime number.
  */
 
 //                                                 _Output Two_
 /*
-Enter a positive integer: 228
+Enter a number: 228
 228 isn't a prime number.
  */
 
 //                                                 _Output Three_
 /*
-Enter a positive integer: -228
-Error: the number should be a positive number.
+Enter a number: -228
+-228 isn't a prime number.
  */
